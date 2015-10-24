e71995646f5508f212472f43cc185135581d6acc
Hugo8705 <contact@hugochaume.com>
2015-10-24 20:15:35 +0200

    First commit

---
+++ js/caroussel.js
@@ -0,0 +1,32 @@
+/*(function($){
+  Drupal.behaviors.some_name = {
+    attach: function (context) {*/
+
+		jQuery(document).ready(function(){
+
+		        jQuery("#caroussel-hugo").carousel();
+                
+                /*jQuery("#caroussel-hugo .carousel-inner img").each(
+                    function() {
+                        var hauteurImage = jQuery(this).height();
+                        var marge = - (hauteurImage/2) + "px";
+                        console.log(marge);
+                        jQuery(this).css({"margin-top": marge});
+                    }
+                );*/
+
+		});
+
+
+
+
+/*
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}*/
